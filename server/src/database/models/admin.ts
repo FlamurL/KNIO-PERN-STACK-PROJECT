9a1fc83bb1dc5b427f5579b6a69f86c0ffce74e6
flimani <flamurlimani004@gmail.com>
2025-04-30 17:15:17 +0200

    db models updated

--- server/src/database/models/admin.ts
+++ server/src/database/models/admin.ts
@@ -3,119 +3,108 @@ import { DataTypes, Model, Optional } from 'sequelize';
 
 export interface adminAttributes {
   id: string;
-  name?: string;
-  surname?: string;
-  email?: string;
-  passwordHash: string;
-  isActive?: boolean;
+  facilityName?: string;
+  facilityAddress?: string;
+  zipCode?: string;
+  country: string;
+  city: string;
+  email: string;
+  password: string;
+  waitingTime: number;
   createdAt: Date;
   updatedAt: Date;
-  type: "ADMIN" | "EMPLOYEE" | "MANAGER";
-  socialSecurityNumber: string;
-  phoneNumber?: string;
-  country?: string;
-  city?: string;
-  address?: string;
 }
 
-export type adminPk = "id";
+export type adminPk = 'id';
 export type adminId = admin[adminPk];
-export type adminOptionalAttributes = "name" | "surname" | "email" | "isActive" | "type" | "phoneNumber" | "country" | "city" | "address";
-export type adminCreationAttributes = Optional<adminAttributes, adminOptionalAttributes>;
+export type adminOptionalAttributes =
+  | 'facilityName'
+  | 'facilityAddress'
+  | 'zipCode';
+export type adminCreationAttributes = Optional<
+  adminAttributes,
+  adminOptionalAttributes
+>;
 
-export class admin extends Model<adminAttributes, adminCreationAttributes> implements adminAttributes {
+export class admin
+  extends Model<adminAttributes, adminCreationAttributes>
+  implements adminAttributes
+{
   id!: string;
-  name?: string;
-  surname?: string;
-  email?: string;
-  passwordHash!: string;
-  isActive?: boolean;
+  facilityName?: string;
+  facilityAddress?: string;
+  zipCode?: string;
+  country!: string;
+  city!: string;
+  email!: string;
+  password!: string;
+  waitingTime!: number;
   createdAt!: Date;
   updatedAt!: Date;
-  type!: "ADMIN" | "EMPLOYEE" | "MANAGER";
-  socialSecurityNumber!: string;
-  phoneNumber?: string;
-  country?: string;
-  city?: string;
-  address?: string;
-
 
   static initModel(sequelize: Sequelize.Sequelize): typeof admin {
-    return admin.init({
-    id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    surname: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    email: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    passwordHash: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    isActive: {
-      type: DataTypes.BOOLEAN,
-      allowNull: true,
-      defaultValue: true
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    type: {
-      type: DataTypes.ENUM("ADMIN","EMPLOYEE","MANAGER"),
-      allowNull: false,
-      defaultValue: "ADMIN"
-    },
-    socialSecurityNumber: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    phoneNumber: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    country: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    city: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    address: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    }
-  }, {
-    sequelize,
-    tableName: 'admin',
-    schema: 'public',
-    timestamps: false,
-    freezeTableName: true,
-    indexes: [
+    return admin.init(
       {
-        name: "admin_pkey",
-        unique: true,
-        fields: [
-          { name: "id" },
-        ]
+        id: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          primaryKey: true,
+        },
+        facilityName: {
+          type: DataTypes.STRING(255),
+          allowNull: true,
+        },
+        facilityAddress: {
+          type: DataTypes.STRING(255),
+          allowNull: true,
+        },
+        zipCode: {
+          type: DataTypes.STRING(255),
+          allowNull: true,
+        },
+        country: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        city: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        password: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        waitingTime: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
       },
-    ]
-  });
+      {
+        sequelize,
+        tableName: 'admin',
+        schema: 'public',
+        timestamps: false,
+        freezeTableName: true,
+        indexes: [
+          {
+            name: 'admin_pkey',
+            unique: true,
+            fields: [{ name: 'id' }],
+          },
+        ],
+      }
+    );
   }
 }
--- server/src/database/models/users.ts
+++ server/src/database/models/users.ts
@@ -1,140 +1,84 @@
 import * as Sequelize from 'sequelize';
 import { DataTypes, Model, Optional } from 'sequelize';
-import {UserType} from '../../dtos/user.dto';
 
 export interface usersAttributes {
   id: string;
   name?: string;
   email?: string;
-  passwordHash?: string;
+  passwordHash: string;
   isActive?: boolean;
   createdAt: Date;
   updatedAt: Date;
-  surname?: string;
-  type?: UserType;
-  socialSecurityNumber?: string;
-  phoneNumber?: string;
-  country?: string;
-  city?: string;
-  address?: string;
 }
 
 export type usersPk = 'id';
 export type usersId = users[usersPk];
-export type usersOptionalAttributes =
-  'id'
-  | 'name'
-  | 'email'
-  | 'passwordHash'
-  | 'createdAt'
-  | 'updatedAt'
-  | 'surname'
-  | 'type'
-  | 'socialSecurityNumber'
-  | 'phoneNumber'
-  | 'country'
-  | 'city'
-  | 'address'
-  ;
-export type usersCreationAttributes = Optional<usersAttributes, usersOptionalAttributes>;
+export type usersOptionalAttributes = 'name' | 'email' | 'isActive';
+export type usersCreationAttributes = Optional<
+  usersAttributes,
+  usersOptionalAttributes
+>;
 
-export class users extends Model<usersAttributes, usersCreationAttributes> implements usersAttributes {
+export class users
+  extends Model<usersAttributes, usersCreationAttributes>
+  implements usersAttributes
+{
   id!: string;
   name?: string;
   email?: string;
-  passwordHash?: string;
+  passwordHash!: string;
   isActive?: boolean;
   createdAt!: Date;
   updatedAt!: Date;
-  surname?: string;
-  type?: UserType;
-  socialSecurityNumber?: string;
-  phoneNumber?: string;
-  country?: string;
-  city?: string;
-  address?: string;
-
 
   static initModel(sequelize: Sequelize.Sequelize): typeof users {
-    return users.init({
-      id: {
-        type: DataTypes.UUID,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-      },
-      name: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-      },
-      email: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-      },
-      passwordHash: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      surname: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-      },
-      type: {
-        type: DataTypes.ENUM(...Object.values(UserType)),
-        allowNull: true,
-      },
-      socialSecurityNumber: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-        validate: {
-          notEmpty: true,
-          len: [10, 16],
-        }
-      },
-      phoneNumber: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-        validate: {
-          len: [5, 15],
-        }
-      },
-      country: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-      },
-      city: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-      },
-      address: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-      }
-    }, {
-      sequelize,
-      tableName: 'Users',
-      schema: 'public',
-      timestamps: true,
-      indexes: [
-        {
-          name: 'Users_pkey',
-          unique: true,
-          fields: [
-            { name: 'id' },
-          ],
+    return users.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          primaryKey: true,
         },
-      ],
-    });
+        name: {
+          type: DataTypes.STRING(255),
+          allowNull: true,
+        },
+        email: {
+          type: DataTypes.STRING(255),
+          allowNull: true,
+        },
+        passwordHash: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        isActive: {
+          type: DataTypes.BOOLEAN,
+          allowNull: true,
+          defaultValue: true,
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        tableName: 'users',
+        schema: 'public',
+        timestamps: false,
+        freezeTableName: true,
+        indexes: [
+          {
+            name: 'users_pkey',
+            unique: true,
+            fields: [{ name: 'id' }],
+          },
+        ],
+      }
+    );
   }
 }
-
-
-export default users;
